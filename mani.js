4200c4cfa76de2576fcaa71da027a86d1e01411e
edgarromerocuc1994 <edgarromerocuc94@gmail.com>
2022-11-19 11:10:54 -0600

    creamos el index

---
+++ mani.js
@@ -0,0 +1,35 @@
+const empresa1 = {
+    a: "1",  
+    b: "2", 
+    c: "3", 
+}
+
+const nombreEmpresa = {
+    a: "Apple",
+    b: "Microsoft",
+    c: "Google",
+
+}
+
+const keyEmpresa = {
+    key1: "true",
+    key2: "false",
+    key3: "undefined",
+
+}
+
+console.log(Object.keys(empresa1))
+console.log(Object.values(empresa1))
+console.log(Object.entries(empresa1))
+
+console.log(Object.keys(nombreEmpresa))
+console.log(Object.values(nombreEmpresa))
+console.log(Object.entries(nombreEmpresa))
+
+console.log(Object.keys(keyEmpresa))
+console.log(Object.values(keyEmpresa))
+console.log(Object.entries(keyEmpresa))
+
+
+
+
